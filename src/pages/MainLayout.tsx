94bd08cc44a8620775a518668be15ff8741c3dba
codeztree <codeztree@naver.com>
2024-09-19 04:49:20 +0900

    chore: set app max width

--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
 const MainLayout = () => {
   return (
-    <div className="flex flex-col w-full h-screen">
-      <Outlet />
+    <div className="flex justify-center">
+      <div className="flex flex-col w-app-width h-screen bg-cntimer-background px-6">
+        <Outlet />
+      </div>
     </div>
   );
 };

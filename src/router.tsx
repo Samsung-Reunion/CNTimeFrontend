95de225058669a4cd26e34b8e154dba3ac77174d
codeztree <codeztree@naver.com>
2024-09-23 00:36:33 +0900

    delete: break timer

--- src/router.tsx
+++ src/router.tsx
@@ -2,7 +2,6 @@ import { RouteObject } from 'react-router-dom';
 import MainLayout from './pages/MainLayout';
 import HomePage from './pages/HomePage';
 import RunTimerPage from './pages/RunTimerPage';
-import BreakTimerPage from './pages/BreakTimerPage';
 import FinishTaskPage from './pages/FinishTaskPage';
 import EnterNamePage from './pages/EnterNamePage';
 import CreateOrJoinPage from './pages/CreateOrJoinPage';
@@ -25,7 +24,6 @@ const routes: RouteObject[] = [
       { path: '/projectname', element: <ProjectNamePage /> },
       { path: '/projectcode', element: <ProjectCodePage /> },
       { path: '/runtimer', element: <RunTimerPage /> },
-      { path: '/breaktimer', element: <BreakTimerPage /> },
       { path: '/finishtask', element: <FinishTaskPage /> },
       { path: '/writegoal', element: <TargetGoalInputPage /> },
     ],

4b2e428f353682aa418289473804429ddbffef6a
arky02 <kyean07@naver.com>
2024-09-30 21:59:34 +0900

    feat: react-hot-toast setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,25 @@
-import { BrowserRouter } from 'react-router-dom';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import { StateProvider } from './StateContext';
-import './index.css';
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { StateProvider } from "./StateContext";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StateProvider>
     <BrowserRouter>
       <App />
+      <Toaster
+        containerStyle={{ fontSize: "16px", fontWeight: "600" }}
+        toastOptions={{
+          style: {
+            padding: "7px",
+            color: "#242424",
+            borderRadius: "999px",
+            fontSize: "13px",
+          },
+        }}
+      />
     </BrowserRouter>
   </StateProvider>
 );

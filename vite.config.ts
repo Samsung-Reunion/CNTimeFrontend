6f181d02ad502c91cbc5d65c0f9b325849773e17
arky02 <kyean07@naver.com>
2024-09-30 21:58:57 +0900

    chore: change vite port to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,24 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@': path.resolve(__dirname, 'src'),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     emptyOutDir: true,
+  },
+  server: {
+    port: 3000, // 원하는 포트 번호로 변경 가능
   },
 });

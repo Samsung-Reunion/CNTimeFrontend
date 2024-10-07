eefb8cd13199fe3ac6973f105045c6e88d06deab
daisyHyeseul <csj0934@naver.com>
2024-10-07 17:44:40 +0900

    fix: api bearer 삭제

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,15 +1,15 @@
-import axios from "axios";
-import Cookies from "js-cookie";
+import axios from 'axios';
+import Cookies from 'js-cookie';
 
 export const instance = axios.create({
-  baseURL: "http://141.164.52.130:8081",
+  baseURL: 'http://141.164.52.130:8081',
   timeout: 5000,
 });
 
 instance.interceptors.request.use(function (config) {
-  const access_token = Cookies.get("access_token") ?? null;
+  const access_token = Cookies.get('access_token') ?? null;
 
-  if (access_token) config.headers.Authorization = `Bearer ${access_token}`;
+  if (access_token) config.headers.Authorization = `${access_token}`;
 
   return config;
 });
